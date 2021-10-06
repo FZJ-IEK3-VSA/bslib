8c1d6cc98be2dfc1d9d0ac7b4782d72820653a39
Kai Rösken <kroesken71@gmail.com>
2021-10-06 15:41:15 +0200

    Update bslib_database.py

--- src/bslib_database.py
+++ src/bslib_database.py
@@ -7,14 +7,13 @@ def read_excel_to_df():
     """This functions reads an Microsoft Excel file and returns it as an pandas data frame"""
     df = pd.read_excel('../input/PerModPAR.xlsx', sheet_name='Data')
 
+    # Drop all columns after End marker
     end = df.loc[:, (df == 'End').any()].columns
-
     end_name = end.values[0]
-
     df = df.loc[:, :end_name]
-
     df = df.iloc[:, :-1]
 
+    # Drop first 4 columns
     df = df.iloc[:, 4:]
 
     df = df.drop('Unit', axis=1)
@@ -31,15 +30,19 @@ def transpose_df(df):
     df = df.iloc[1:, :]
     df = df.iloc[:, 1:]
 
+    # Drop columns which are empty
     df = df.loc[:, df.columns.notnull()]
 
+    # Fill emtpy cells with nan
     df = df.replace(r'^\s+$', np.nan, regex=True)
 
     return df
 
-def edit_dataframe(df):
+
+def drop_columns(df):
 
     drop_cols = [
+        "Type",
         "p_PV2AC_5",
         "p_PV2AC_10",
         "p_PV2AC_20",
@@ -126,25 +129,70 @@ def edit_dataframe(df):
         'Pro3',
         'Info',
         'Cat'
-
     ]
 
     df = df.drop(drop_cols, axis=1)
 
-    df['Top'] = df['Type']
-
     return df
+
+
+def rename_columns(df):
+    renamed_cols = {"Man1": "Manufacturer (PE)",
+                    "Pro1": "Model (PE)",
+                    "Man2": "Manufacturer (BAT)",
+                    "Pro2": "Model (BAT)",
+                    "Top": "Type [-coupled]",
+                    'P_PV2AC_in': 'P_PV2AC_in [W]',
+                    'P_PV2AC_out': 'P_PV2AC_out [W]',
+                    'U_PV_min': 'U_PV_min [V]',
+                    'U_PV_nom': 'U_PV_nom [V]',
+                    'U_PV_max': 'U_PV_max [V]',
+                    'U_MPP_min': 'U_MPP_min [V]',
+                    'U_MPP_max': 'U_MPP_max [V]',
+                    'P_AC2BAT_in': 'P_AC2BAT_in [W]',
+                    'P_BAT2AC_out': 'P_BAT2AC_out [W]',
+                    'P_PV2BAT_in': 'P_PV2BAT_in [W]',
+                    'P_BAT2PV_out': 'P_BAT2PV_out [W]',
+                    'P_PV2BAT_out': 'P_PV2BAT_out [W]',
+                    'P_BAT2AC_in': 'P_BAT2AC_in [W]',
+                    'U_BAT_min': 'U_BAT_min [V]',
+                    'U_BAT_nom': 'U_BAT_nom [V]',
+                    'U_BAT_max': 'U_BAT_max [V]',
+                    'E_BAT_100': 'E_BAT_100 [kWh]',
+                    'E_BAT_50': 'E_BAT_50 [kWh]',
+                    'E_BAT_25': 'E_BAT_25 [kWh]',
+                    'E_BAT_usable': 'E_BAT_usable [kWh]',
+                    'eta_BAT_100': 'eta_BAT_100',
+                    'eta_BAT_50': 'eta_BAT_50',
+                    'eta_BAT_25': 'eta_BAT_25',
+                    'eta_BAT': 'eta_BAT',
+                    'P_SYS_SOC1_AC': 'P_SYS_SOC1_AC [W]',
+                    'P_SYS_SOC1_DC': 'P_SYS_SOC1_DC [W]',
+                    'P_SYS_SOC0_AC': 'P_SYS_SOC0_AC [W]',
+                    'P_SYS_SOC0_DC': 'P_SYS_SOC0_DC [W]',
+                    'P_PVINV_AC': 'P_PVINV_AC [W]',
+                    'P_PERI_AC': 'P_PERI_AC [W]',
+                    'P_PV2BAT_DEV_IMPORT': 'P_PV2BAT_DEV_IMPORT [W]',
+                    'P_PV2BAT_DEV_EXPORT': 'P_PV2BAT_DEV_EXPORT [W]',
+                    'P_BAT2AC_DEV_IMPORT': 'P_BAT2AC_DEV_IMPORT [W]',
+                    'P_BAT2AC_DEV_EXPORT': 'P_BAT2AC_DEV_EXPORT [W]',
+                    't_DEAD': 't_DEAD [s]',
+                    't_SETTLING': 't_SETTLING [s]'
+                    }
+
+    return df.rename(columns=renamed_cols)
+
+
+def export_to_csv(df):
+    df.to_csv('../input/out.csv', index=False)
 
 
 def main():
     df = read_excel_to_df()
     df = transpose_df(df)
-    df = edit_dataframe(df)
+    df = drop_columns(df)
+    df = rename_columns(df)
     export_to_csv(df)
-
-
-def export_to_csv(df):
-    df.to_csv('../input/out.csv', index=False, header=None)
 
 
 if __name__ == "__main__":
